a3791496eed317a83c88a3b78846376d4312e4df
demobo-com <jeff@demobo.com>
2014-11-08 10:55:57 -0800

    basic api.ai query

--- assets/www/js/configs/Helper.js
+++ assets/www/js/configs/Helper.js
@@ -15,7 +15,27 @@ define(function (require, exports, module) {
         },
 
         processApiAi: function(text, successCallback, failCallback) {
-            var accessToken = "70d60f56c6364637b8c094b1e08ab613";
+            var accessToken = "e18d92c0af64436990f7ec387a200e85";
+            var subscriptionKey = "c0b63dd4-bc77-448d-95ad-d9ed87729ad4";
+            var baseUrl = "https://api.api.ai/v1/";
+            $.ajax({
+                type: "POST",
+                url: baseUrl + "query/",
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                headers: {
+                    "Authorization": "Bearer " + accessToken,
+                    "ocp-apim-subscription-key": subscriptionKey
+                },
+                data: JSON.stringify({ q: text, lang: "en" }),
+
+                success: successCallback,
+                error: failCallback
+            });
+        },
+
+        processToyota: function(text, successCallback, failCallback) {
+            var accessToken = "e18d92c0af64436990f7ec387a200e85";
             var subscriptionKey = "c0b63dd4-bc77-448d-95ad-d9ed87729ad4";
             var baseUrl = "https://api.api.ai/v1/";
             $.ajax({
